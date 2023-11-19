40a4889d1ac504ecd757ecdc3a7898e8984cd60e
Oke Babatunde <akurracy@gmail.com>
2023-11-19 21:33:56 +0100

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://35.88.157.137:5000";
+export const host = "localhost:5000";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
