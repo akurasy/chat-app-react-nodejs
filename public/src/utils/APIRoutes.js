63681c3f15cf3cdd30d4efe1115373f50006579b
akuracy <akurracy@gmail.com>
2023-11-19 20:30:43 +0000

    first commit

---
+++ public/src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "http://35.88.157.137:5000";
+export const loginRoute = `${host}/api/auth/login`;
+export const registerRoute = `${host}/api/auth/register`;
+export const logoutRoute = `${host}/api/auth/logout`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const recieveMessageRoute = `${host}/api/messages/getmsg`;
+export const setAvatarRoute = `${host}/api/auth/setavatar`;
